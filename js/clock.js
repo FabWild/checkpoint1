8d7fed4ca6959b703eaeb08ee93091017153f43a
FabWild <fab@dfweb.fr>
2017-09-29 12:35:29 +0200

    premier essai clock

---
+++ js/clock.js
@@ -0,0 +1,7 @@
+//new Date().getHours(),":",new Date().getMinutes(),":",new Date().getSeconds()
+
+
+setInterval(function(){
+//  $('div class="clock"').write('<div class="clock">');
+  document.getElementById(myclock).write('test');
+}, 1000);
